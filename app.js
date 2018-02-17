5e66e946c6bdaeebb9fa7c64a3ce18abd4e32c45
Shurakshya Kharel <kharel.shurakshya@gmail.com>
2018-02-17 18:13:51 +0200

    error handled for db

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const ApiBuilder = require('claudia-api-builder'),
   mongoose = require('mongoose'),
   api = new ApiBuilder();
-require('./model/db');
+
+/* connect to db */
+const dbURI = `mongodb://${process.env.DATABASE_USER}:${
+  process.env.DATABASE_PASSWORD
+}@ds155747.mlab.com:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+
+mongoose.connect(dbURI);
+
 const todosCtrl = require('./controller/todosCtrl');
 
-
 /* api */
-api.get('/hello', function () {
+api.get('/hello', function() {
   return 'hello I am shura';
 });
 
-api.get('/greet', function (request) {
+api.get('/greet', function(request) {
   const superb = require('superb');
   return request.queryString.name + ' is ' + superb();
 });
 
 /* database routes execution */
-api.get('/todos',todosCtrl.getTodos);
-api.get('/todo/{id}',todosCtrl.getTodo);
-
+api.get('/todos', todosCtrl.getTodos);
+api.get('/todo/{id}', todosCtrl.getTodo);
 
 module.exports = api;
-
--- controller/todosCtrl.js
+++ controller/todosCtrl.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('../model/todos');
 const db = mongoose.model('Todos');
 
 // get all todos
